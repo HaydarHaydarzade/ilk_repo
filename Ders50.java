3a5cd277fe8af6602c0e5cf974d76a853271c709
HaydarHaydarzade <h.haydarzade1987@gmail.com>
2024-10-15 20:41:41 +0400

    dovr sayi 100 oldu

--- Ders50.java
+++ Ders50.java
@@ -1,7 +1,7 @@
 public class Ders50 {
 
     public static void main(String[] args) {
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i <100; i++) {
            System.out.println("Salam Dunya"); 
         }
         
