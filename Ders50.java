d1cda57c3b6f0fbd1c91be8be3fa7cfd33e45be7
HaydarHaydarzade <h.haydarzade1987@gmail.com>
2024-10-15 19:46:40 +0400

    dovr elave edildi

--- Ders50.java
+++ Ders50.java
@@ -1,6 +1,9 @@
 public class Ders50 {
 
     public static void main(String[] args) {
-        System.out.println("Salam Dunya");
+        for (int i = 0; i <10; i++) {
+           System.out.println("Salam Dunya"); 
+        }
+        
     }
 }
